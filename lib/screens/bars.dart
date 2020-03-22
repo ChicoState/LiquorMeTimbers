0b0daa57c7f40f58b4c10c37fe896d03077acd6f
Eric Kiesow <ekiesow@mail.csuchico.edu>
2020-03-21 21:01:53 -0700

    Added expandedTile widget and address info to bars page

--- lib/screens/bars.dart
+++ lib/screens/bars.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class BarsPage extends StatefulWidget {
   @override
@@ -25,9 +27,36 @@ class _BarsPageState extends State<BarsPage> {
   }
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
-    return ListTile(
-      title: Text(
-        document['name'],
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12.0)),
+      ),
+      elevation: 2,
+      margin: EdgeInsets.all(8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ExpansionTile(
+          title: Text(
+            document['name'],
+          ),
+          trailing: SizedBox(),
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(document['address']['street']),
+                Row(
+                  children: <Widget>[
+                    Text('${document['address']['city']}, '),
+                    Text('${document['address']['state']} '),
+                    Text(document['address']['zip']),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
