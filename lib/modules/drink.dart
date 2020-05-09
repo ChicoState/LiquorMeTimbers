a256b8d8e8ade2f70622d4e2ad018284c74be919
Eric <ekiesow@mail.csuchico.edu>
2020-05-09 14:55:40 -0700

    Added Drink class for objects from the db

---
+++ lib/modules/drink.dart
@@ -0,0 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+///
+/// Drink class
+/// creates an object from data in Cloud Firestore database
+///
+class Drink {
+  final String name;
+  final int favoriteCount;
+  final bool happyHour;
+  final String price;
+  final Category category;
+
+  Drink({
+    this.name,
+    this.favoriteCount,
+    this.happyHour,
+    this.price,
+    this.category,
+  });
+
+  factory Drink.fromFirestore(DocumentSnapshot documentSnapshot) {
+    Map data = documentSnapshot.data;
+    return Drink(
+      name: data['name'] ?? '',
+      favoriteCount: data['favorite_count'] ?? 0,
+      happyHour: data['happy_hour'] ?? false,
+      price: data['price'] ?? '',
+      category: data['Category'],
+    );
+  }
+}
+
+///
+/// Category class
+/// category object from Cloud Firestore data for each Drink
+///
+class Category {
+  final bool rum;
+  final bool vodka;
+  final bool tequila;
+  final bool whiskey;
+
+  Category({
+    this.rum,
+    this.vodka,
+    this.tequila,
+    this.whiskey,
+  });
+
+  factory Category.fromMap(Map data) {
+    return Category(
+      rum: data['rum'] ?? false,
+      vodka: data['vodka'] ?? false,
+      tequila: data['tequila'] ?? false,
+      whiskey: data['whiskey'] ?? false,
+    );
+  }
+}
--- lib/services/db.dart
+++ lib/services/db.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:liquor/modules/bar.dart';
+import 'package:liquor/modules/drink.dart';
 
 class DatabaseService {
   final Firestore _db = Firestore.instance;
@@ -8,5 +9,11 @@ class DatabaseService {
   Stream<List<Bar>> barStream() {
     return _db.collection('bars').snapshots().map(
         (list) => list.documents.map((doc) => Bar.fromFirestore(doc)).toList());
+  }
+
+  /// Get a stream of a list of drinks
+  Stream<List<Drink>> drinkStream() {
+    return _db.collection('drinks').snapshots().map((list) =>
+        list.documents.map((doc) => Drink.fromFirestore(doc)).toList());
   }
 }
