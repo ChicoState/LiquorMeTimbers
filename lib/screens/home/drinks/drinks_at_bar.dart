dc36d08ff38b9c0f2d0baaefa28815f38c097c7d
Eric <ekiesow@mail.csuchico.edu>
2020-05-09 20:40:31 -0700

    Complete drinks list for bars

--- lib/screens/home/drinks/drinks_at_bar.dart
+++ lib/screens/home/drinks/drinks_at_bar.dart
@@ -10,21 +10,25 @@ class DrinksAtBar extends StatelessWidget {
   final Bar bar;
 
   DrinksAtBar({
-    this.bar,
+    @required this.bar,
   });
 
   @override
   Widget build(BuildContext context) {
     return StreamProvider<List<Drink>>.value(
       value: db.drinkStream(),
-      child: DrinksAtBarView(),
+      child: DrinksAtBarView(
+        bar: bar,
+      ),
     );
   }
 }
 
 class DrinksAtBarView extends StatelessWidget {
   final Bar bar;
-  DrinksAtBarView({this.bar});
+  DrinksAtBarView({
+    @required this.bar,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -34,8 +38,11 @@ class DrinksAtBarView extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Card(
+            color: Colors.red,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(kCardRadius)),
+              borderRadius: BorderRadius.all(
+                Radius.circular(kCardRadius),
+              ),
             ),
             elevation: kCardElevation,
             margin: EdgeInsets.all(kCardMargin),
@@ -44,14 +51,19 @@ class DrinksAtBarView extends StatelessWidget {
               child: ListTile(
                 title: Text(
                   bar.name,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
           ),
-          ListView.builder(
-            itemCount: drinks.length,
-            itemBuilder: (context, index) =>
-                _buildListItem(context, drinks[index]),
+          Expanded(
+            child: ListView.builder(
+              itemCount: drinks.length,
+              itemBuilder: (context, index) =>
+                  _buildListItem(context, drinks[index]),
+            ),
           ),
         ],
       ),
@@ -63,36 +75,99 @@ class DrinksAtBarView extends StatelessWidget {
     String price = drink.price;
     Category category = drink.category;
 
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(kCardRadius),
-        ),
-      ),
-      elevation: kCardElevation,
-      margin: EdgeInsets.all(kCardMargin),
-      child: Padding(
-        padding: const EdgeInsets.all(kCardPadding),
-        child: drinkExpansionTile(
-          name,
-          price,
-          category,
-        ),
-      ),
-    );
+    // check for null
+    if (bar.drinks[drink.documentID] == null) return SizedBox();
+
+    // return a Card if the drink exist at the bar otherwise return SizedBox
+    return bar.drinks[drink.documentID]
+        ? Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(kCardRadius),
+              ),
+            ),
+            elevation: kCardElevation,
+            margin: EdgeInsets.all(kCardMargin),
+            child: Padding(
+              padding: const EdgeInsets.all(kCardPadding),
+              child: drinkExpansionTile(
+                name,
+                price,
+                category,
+              ),
+            ),
+          )
+        : SizedBox();
   }
 
-  Widget drinkExpansionTile(
-    String name,
-    String price,
-    Category category,
-  ) {
+  Widget drinkExpansionTile(String name, String price, Category category) {
+    List<Widget> categoryWidget = categories(category);
     return ExpansionTile(
       title: Text(
         name,
       ),
       trailing: SizedBox(),
-      children: <Widget>[],
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 12.0, bottom: 12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Price: $price',
+              ),
+              categoryWidget.length == 1
+                  ? Text(
+                      'Category: ',
+                    )
+                  : Text(
+                      'Categories: ',
+                    ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: categoryWidget,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  List<Widget> categories(Category category) {
+    List<Widget> categoryList = [];
+    if (category.rum) {
+      Widget text = categoryText('Rum');
+      categoryList.add(
+        text,
+      );
+    }
+    if (category.vodka) {
+      Widget text = categoryText('Vodka');
+      categoryList.add(
+        text,
+      );
+    }
+    if (category.tequila) {
+      Widget text = categoryText('Tequila');
+      categoryList.add(
+        text,
+      );
+    }
+    if (category.whiskey) {
+      Widget text = categoryText('Tequila');
+      categoryList.add(
+        text,
+      );
+    }
+    return categoryList;
+  }
+
+  Widget categoryText(String text) {
+    return Text(
+      text,
     );
   }
 }
